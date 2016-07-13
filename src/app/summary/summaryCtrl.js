ab2ad7d215e0724bb0413e1f911ff6efb7abc7e6
myjournal <getmyjournal@gmail.com>
2016-07-13 16:46:49 +0000

    view summary page creation

--- src/app/summary/summaryCtrl.js
+++ src/app/summary/summaryCtrl.js
@@ -1,15 +1,42 @@
 (function() {
-	'use strict';
-	angular
-		.module('appraisalApp')
-		.controller('SummaryCtrl', Summary);
+    'use strict';
+    angular
+        .module('appraisalApp')
+        .controller('SummaryCtrl', Summary);
 
+    function Summary() {
+        /*jshint validthis: true */
 
-	function Summary() {
-		/*jshint validthis: true */
-		var vm = this;
-		vm.title = "View Summary";
-		return vm;
-	}
+        var employeeList = [{
+            "employeeID": "1",
+            "name": "Thomas",
+            "overallScore": 80
+        }, {
+            "employeeID": 2,
+            "name": "Maya",
+            "overallScore": 80
+        }];
+
+        var vm = this;
+        vm.title = "View Summary";
+        var tabs = [{
+            list: employeeList
+        }];
+        vm.tabs = tabs;
+        vm.selectedIndex = 0;
+        vm.onTabSelected = function(tab) {
+            vm.isDrilldownView = vm.tabs.length <= 1 ? false : true;
+        };
+        vm.addTab = function(employee) {
+            tabs.push({
+                list: employee
+            });
+        };
+        vm.removeTab = function(tab) {
+            var index = tabs.indexOf(tab);
+            tabs.splice(index, 1);
+        };
+        return vm;
+    }
 
 })();
